e9646511a1d754df395d417afc57feae05b1bd8c
AlfUrl-indtesa <alfonso_rodriguez@indtesa-electronica.com>
2026-05-05 11:57:38 -0600

    Mejoras Report

--- common/data/src/main/java/org/thingsboard/server/common/data/report/ReportEntityFilter.java
+++ common/data/src/main/java/org/thingsboard/server/common/data/report/ReportEntityFilter.java
@@ -3,22 +3,20 @@ package org.thingsboard.server.common.data.report;
 import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Data;
 import org.thingsboard.server.common.data.id.EntityId;
-import org.thingsboard.server.common.data.id.EntityGroupId;
 
-import javax.validation.constraints.NotNull;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class ReportEntityFilter {
 
-    @NotNull
     private ReportScopeType scopeType;
 
     private String entityType;
 
     private List<EntityId> entityIds;
 
-    private EntityGroupId entityGroupId;
+    private UUID entityGroupId;
 
     private JsonNode criteria;
 }
